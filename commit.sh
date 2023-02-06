f2f3ac7e32f142489db8ae6cf9fbf194ab02a239
Stefan Lew <stefan.lew@hae.sh>
2023-02-06 18:34:37 +0100

    fix: typos in curl script

--- commit.sh
+++ commit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/json" \
-    -h "'$HAESH_STREAM_HEADER_NAME': '$HAESH_STREAM_HEADER_VALUE"
-    -d '{"sha":"'$GITHUB_SHA'", "ref":"'$GITHUB_REF'"}' \
-    https://api.dice.staging.hae.sh/streams/$HAESH_STREAM_ID
+curl --request POST \
+    --header "Content-Type: application/json" \
+    --header "'$HAESH_STREAM_HEADER_NAME': '$HAESH_STREAM_HEADER_VALUE'" \
+    --data '{"sha":"'$GITHUB_SHA'", "ref":"'$GITHUB_REF'"}' \
+    --url https://api.dice.staging.hae.sh/streams/$HAESH_STREAM_ID
