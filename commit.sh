26ac18cfc17f81112f57bb4d8312a3a17588511f
Stefan Lew <stefan.lew@hae.sh>
2023-02-07 09:08:48 +0100

    fix: variables in shell script

--- commit.sh
+++ commit.sh
@@ -2,6 +2,6 @@
 
 curl --request POST \
     --header "Content-Type: application/json" \
-    --header "'$HAESH_STREAM_HEADER_NAME': '$HAESH_STREAM_HEADER_VALUE'" \
+    --header "$HAESH_STREAM_HEADER_NAME: $HAESH_STREAM_HEADER_VALUE" \
     --data '{"sha":"'$GITHUB_SHA'", "ref":"'$GITHUB_REF'"}' \
     --url https://api.dice.staging.hae.sh/streams/$HAESH_STREAM_ID
