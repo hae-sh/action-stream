7611cfbf7374dd00e5030d443f7c4313a43b50b2
Stefan Lew <stefan.lew@hae.sh>
2023-02-07 10:26:09 +0100

    feat: add CID calculation for git commit

--- commit.sh
+++ commit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+COMMIT_CID=$(node utils/shaToCid.js $GITHUB_SHA)
+
 curl --request POST \
     --header "Content-Type: application/json" \
     --header "$HAESH_STREAM_HEADER_NAME: $HAESH_STREAM_HEADER_VALUE" \
-    --data '{"sha":"'$GITHUB_SHA'", "ref":"'$GITHUB_REF'"}' \
+    --data '{"sha":"'$GITHUB_SHA'", "ref":"'$GITHUB_REF'", \
+             "cid":{"/":"'$COMMIT_CID'"}}' \
     --url https://api.dice.staging.hae.sh/streams/$HAESH_STREAM_ID
---
+++ utils/shaToCid.js
@@ -0,0 +1,25 @@
+import { CID } from 'multiformats/cid';
+import { create } from 'multiformats/hashes/digest';
+
+/**
+ * Converts a git sha1 hash to a CID
+ *
+ * @param hexDigest {string} - the hex string of the sha1 hash
+ * @returns {CID} - the CID
+ */
+function shaToCid(hexDigest) {
+  const buf = Buffer.from(hexDigest, 'hex');
+  // 0x11 is the code for sha1
+  const mh = create(0x11, buf);
+  // 0x78 is the code for git-raw
+  return CID.create(1, 0x78, mh);
+}
+
+const inputSha = process.argv[2];
+
+if (!inputSha) {
+  console.log('Usage: node shaToCid.js <sha>');
+  process.exit(1);
+}
+
+console.log(shaToCid(inputSha).toString());
