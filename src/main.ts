e95ee9858f46e7d8eacea7ea97052ea2c9b16e64
Stefan Lew <stefan.lew@hae.sh>
2023-02-07 13:54:18 +0100

    feat: re-implement stream logic

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,38 @@
 import * as core from "@actions/core";
 import { shaToCid } from "./shaToCid";
+import axios from "axios";
 
 async function run(): Promise<void> {
   try {
-    core.debug(JSON.stringify(process.env, null, 2));
-    // const sha = core.getInput("sha");
-    // const cid = shaToCid(sha);
-    // core.setOutput("cid", cid.toString());
+    const sha = process.env["GITHUB_SHA"];
+    const ref = process.env["GITHUB_REF"];
+
+    if (!sha) throw new Error("GITHUB_SHA is not set");
+    if (!ref) throw new Error("GITHUB_REF is not set");
+
+    const cid = shaToCid(sha);
+
+    const data = {
+      cid: { "/": cid.toString() },
+      sha,
+      ref,
+    } as const;
+
+    const result = await axios.post(
+      "http://api.dice.staging.hae.sh/streams/" +
+        core.getInput("haesh_stream_id"),
+      data,
+      {
+        headers: {
+          [core.getInput("haesh_stream_header_name")]: core.getInput(
+            "haesh_stream_header_value"
+          ),
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    core.setOutput("result", JSON.stringify(result.data));
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
